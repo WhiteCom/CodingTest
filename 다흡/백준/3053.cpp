f62c5658cfe942518ee0cb613517c850f229f564
WhiteCom <vip1408@naver.com>
2021-03-28 02:22:20 +0900

    Add 3053.cpp

---
+++ 다흡/백준/3053.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <math.h>
+#define PI 3.14159265358979323846
+int main()
+{
+	int r;
+	scanf("%d", &r);
+	double u_circle = (double)(r * r * PI);
+	double t_w = (double)(2 * r * r);
+	double t_circle = t_w * t_w;
+	printf("%lf\n%lf\n", u_circle, t_circle);
+	return 0;
+}

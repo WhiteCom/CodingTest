8fb151d08f742f7834b547137f3b83eb4c3ab2a9
WhiteCom <vip1408@naver.com>
2021-03-28 23:57:53 +0900

    add 2839.cpp

---
+++ 다흡/백준/2839.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#define MAX 9999
+
+int min(int a, int b)
+{
+	return a < b ? a : b;
+}
+
+int main()
+{
+	int inp;
+	scanf("%d", &inp);
+	int* arr = new int[5001];
+
+	for (int i = 0; i < 5001; i++)
+		arr[i] = MAX;
+	
+	arr[3] = 1;
+	arr[5] = 1;
+
+	for (int i = 6; i <= 5000; i++)
+	{
+		arr[i] = min(arr[i - 3] + 1, arr[i - 5] + 1);
+	}
+
+	if (arr[inp] < MAX)
+		printf("%d\n", arr[inp]);
+	else
+		printf("-1\n");
+
+	delete arr;
+	return 0;
+}

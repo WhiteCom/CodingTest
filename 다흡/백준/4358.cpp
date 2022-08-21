ade0614a21be0d3cc0ee84e8c67494ef72340d78
WhiteCom <vip1408@naver.com>
2022-08-22 01:04:56 +0900

    백준 4358추가

---
+++ 다흡/백준/4358.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	map<string, int> tree_data;
+	int totalNum = 0;
+
+	// input
+	string input;
+	while (getline(cin, input))
+	{
+		tree_data[input]++;
+		totalNum++;
+	}
+
+	map<string, int>::iterator it;
+	
+	// 소수점 4자리
+	cout << fixed;
+	cout.precision(4);
+	for (it = tree_data.begin(); it != tree_data.end(); ++it)
+	{
+		float num = static_cast<float>(it->second) / totalNum * 100;
+		cout << it->first << " " << num << '\n';
+	}
+
+	return 0;
+}

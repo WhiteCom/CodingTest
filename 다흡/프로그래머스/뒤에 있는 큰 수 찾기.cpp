a107de000bc5c7e4faae1428568046dd6d0999ee
WhiteCom <vip1408@naver.com>
2023-01-28 17:00:18 +0900

    프로그래머스 '뒤에있는 큰 수 찾기' 해결

---
+++ 다흡/프로그래머스/뒤에 있는 큰 수 찾기.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer(numbers.size(), -1);
+
+    // solution 2 : 스택 이용
+    stack<int> index_stack;
+
+    for (int i = 0; i < numbers.size(); ++i)
+    {
+        if (index_stack.empty() || numbers[i] < numbers[i - 1])
+        {
+            index_stack.push(i);
+        }
+        else
+        {
+            while (!index_stack.empty() && numbers[index_stack.top()] < numbers[i])
+            {
+                int nextNum = index_stack.top();
+                index_stack.pop();
+                answer[nextNum] = numbers[i];
+            }
+            index_stack.push(i);
+        }
+    }
+
+    return answer;
+}

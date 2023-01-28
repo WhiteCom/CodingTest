6006d23eaee39432e00abbf40102cfd0428836dc
WhiteCom <vip1408@naver.com>
2023-01-28 13:43:00 +0900

    프로그래머스 푸드파이터 해결

---
+++ 다흡/프로그래머스/푸드파이터.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+
+    deque<char> balancedFood;
+    balancedFood.push_back('0');
+
+    // check food
+    for (int i = food.size() - 1; i > 0; --i)
+    {
+        int mok = food[i] / 2;
+        if (mok > 0)
+        {
+            for (int j = 0; j < mok; ++j)
+            {
+                balancedFood.push_front(i + '0');
+                balancedFood.push_back(i + '0');
+            }
+        }
+    }
+
+    for (int i = 0; i < balancedFood.size(); ++i)
+    {
+        answer.push_back(balancedFood[i]);
+    }
+
+    return answer;
+}

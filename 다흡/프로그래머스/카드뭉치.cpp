7469b20e73c9370098850a7d11b9a14f054c3e83
WhiteCom <vip1408@naver.com>
2023-02-22 00:55:25 +0900

    프로그래머스 연습문제 카드뭉치 해결

---
+++ 다흡/프로그래머스/카드뭉치.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// URL : https://school.programmers.co.kr/learn/courses/30/lessons/159994
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "";
+
+    int count = 0;
+
+    int one_pointer = 0; // 1번 카드뭉치 index
+    int two_pointer = 0; // 2번 카드뭉치 index
+    for (int i = 0; i < goal.size();)
+    {
+        if (goal[i] == cards1[one_pointer] && one_pointer < cards1.size())
+        {
+            count++;
+            one_pointer++;
+        }
+
+        if (goal[i] == cards2[two_pointer] && two_pointer < cards2.size())
+        {
+            count++;
+            two_pointer++;
+        }
+
+        ++i;
+    }
+
+    if (count == goal.size())
+        answer = "Yes";
+    else
+        answer = "No";
+
+    return answer;
+}

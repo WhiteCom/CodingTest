0fc9a6dcada301be2b35790552fce01fe905fbbe
WhiteCom <vip1408@naver.com>
2023-03-01 00:24:41 +0900

    프로그래머스- 크기가 작은 부분 문자열 해결

---
+++ 다흡/프로그래머스/크기가 작은 부분문자열.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string t, string p) {
+    int answer = 0;
+
+    int last_len = t.length() - p.length() + 1;
+    int p_len = p.length();
+    long long p_num = stoll(p);
+
+    for (int i = 0; i < last_len; ++i)
+    {
+        string sub = t.substr(i, p_len);
+        long long num = stoll(sub);
+        if (num <= p_num)
+        {
+            answer++;
+        }
+    }
+
+    return answer;
+}

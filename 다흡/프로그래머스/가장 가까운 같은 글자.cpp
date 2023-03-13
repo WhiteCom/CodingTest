6e92342cef396a1e9a35fcbbdfe95d71a3466017
WhiteCom <vip1408@naver.com>
2023-03-13 18:38:21 +0900

    프로그래머스 가장 가까운 같은 글자 해결

---
+++ 다흡/프로그래머스/가장 가까운 같은 글자.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+
+    map<char, int> data;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (data.find(s[i]) == data.end())
+        {
+            data[s[i]] = i;
+            answer.push_back(-1);
+        }
+        else
+        {
+            int test = i - data[s[i]];
+            data[s[i]] = i;
+            answer.push_back(test);
+        }
+
+    }
+
+    return answer;
+}

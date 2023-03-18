0f510120b4bcfc774acdd3235aaf8d73e07ba80a
WhiteCom <vip1408@naver.com>
2023-03-18 10:58:19 +0900

    프로그래머스 콜라문제 해결

---
+++ 다흡/프로그래머스/콜라문제.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+
+    int bottle = n;
+    int getBottle = 0;
+
+    while (bottle >= a)
+    {
+        getBottle = bottle / a * b;
+        bottle = bottle - (bottle / a * a) + getBottle;
+        // cout << bottle << " " << getBottle << '\n';
+        answer += getBottle;
+    }
+    cout << bottle;
+
+    return answer;
+}

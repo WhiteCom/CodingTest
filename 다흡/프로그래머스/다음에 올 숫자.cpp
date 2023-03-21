eb49509354cd462a7cf3577ae296ab00c37f8990
WhiteCom <vip1408@naver.com>
2023-03-22 00:36:50 +0900

    프로그래머스 다음에올 숫자 해결

---
+++ 다흡/프로그래머스/다음에 올 숫자.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> common) {
+    int answer = 0;
+
+    int commonSize = common.size();
+
+    int d = 0;
+    int r = 0;
+
+    d = common[1] - common[0];
+    r = common[1] / common[0];
+
+    if (common[0] + (commonSize - 1) * d == common[commonSize - 1])
+        answer = common[commonSize - 1] + d;
+    else
+        answer = common[commonSize - 1] * r;
+
+    return answer;
+}

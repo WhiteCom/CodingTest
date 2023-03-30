720a8cd8994271324797146ea9f2e520e94a30fa
WhiteCom <vip1408@naver.com>
2023-03-30 22:35:56 +0900

    프로그래머스 추억점수 해결

---
+++ 다흡/프로그래머스/추억점수.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+
+    map<string, int> data;
+
+    // 현재 name, yearning 이 같다고 가정하고 진행
+    for (int i = 0; i < name.size(); ++i)
+    {
+        data[name[i]] = yearning[i];
+    }
+
+    for (int i = 0; i < photo.size(); ++i)
+    {
+        int sum = 0;
+        for (int j = 0; j < photo[i].size(); ++j)
+        {
+            sum += data[photo[i][j]];
+        }
+        answer.push_back(sum);
+    }
+
+    return answer;
+}

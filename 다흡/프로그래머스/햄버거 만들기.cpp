3bac0686f1742166402e1ab7741c0b9b7932451e
WhiteCom <vip1408@naver.com>
2023-04-26 00:36:46 +0900

    프로그래머스 햄버거 만들기 해결

---
+++ 다흡/프로그래머스/햄버거 만들기.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+
+    vector<int> test_stack;
+
+    // 1, 2, 3, 1 순서
+    for (int i = 0; i < ingredient.size(); ++i)
+    {
+        test_stack.push_back(ingredient[i]);
+        if (test_stack.size() > 3)
+        {
+            int tSize = test_stack.size();
+
+            if (test_stack[tSize - 4] == 1
+                && test_stack[tSize - 3] == 2
+                && test_stack[tSize - 2] == 3
+                && test_stack[tSize - 1] == 1)
+            {
+                answer++;
+                test_stack.pop_back();
+                test_stack.pop_back();
+                test_stack.pop_back();
+                test_stack.pop_back();
+            }
+        }
+    }
+
+    return answer;
+}

890c44fb6a8950e7b888b91f7068abb776d62175
WhiteCom <vip1408@naver.com>
2023-04-28 00:38:58 +0900

    프로그래머스 숫자짝궁 해결

---
+++ 다흡/프로그래머스/숫자짝궁.cpp
@@ -0,0 +1,54 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string X, string Y) {
+    string answer = "";
+
+    vector<int> X_arr(10, 0);
+    vector<int> Y_arr(10, 0);
+
+    vector<int> Re_arr(10, 0);
+
+    for (int i = 0; i < X.length(); ++i)
+    {
+        int num = X[i] - '0';
+        X_arr[num]++;
+    }
+
+    for (int i = 0; i < Y.length(); ++i)
+    {
+        int num = Y[i] - '0';
+        Y_arr[num]++;
+    }
+
+    for (int i = 0; i < 10; ++i)
+    {
+        if (X_arr[i] == 0 || Y_arr[i] == 0)
+            continue;
+
+        int minCnt = X_arr[i] > Y_arr[i] ? Y_arr[i] : X_arr[i];
+        cout << minCnt << '\n';
+        Re_arr[i] = minCnt;
+    }
+
+    for (int i = 9; i > -1; --i)
+    {
+        if (Re_arr[i] > 0)
+        {
+            for (int j = 0; j < Re_arr[i]; ++j)
+            {
+                answer += (i + '0');
+                if (i == 0 && answer.length() < 2) break;
+            }
+        }
+    }
+
+    if (answer.empty())
+        answer = "-1";
+
+
+    return answer;
+}

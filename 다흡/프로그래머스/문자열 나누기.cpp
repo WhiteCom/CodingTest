69e4f3c5d49fb3528b504933d3aefdd480e94827
WhiteCom <vip1408@naver.com>
2023-05-23 00:15:53 +0900

    프로그래머스 문자열나누기 해결

---
+++ 다흡/프로그래머스/문자열 나누기.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+
+    int cnt1 = 0;
+    int cnt2 = 0;
+
+    char k;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (cnt1 == cnt2)
+        {
+            answer += 1;
+            k = s[i];
+        }
+        if (k == s[i])
+        {
+            cnt1 += 1;
+        }
+        else
+        {
+            cnt2 += 1;
+        }
+    }
+
+    return answer;
+}

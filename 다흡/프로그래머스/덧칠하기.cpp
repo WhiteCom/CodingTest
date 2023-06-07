94ee493a68c3685a7e0e51abf9320ebeb526452b
WhiteCom <vip1408@naver.com>
2023-06-08 00:41:38 +0900

    프로그래머스 덧칠하기 해결

---
+++ 다흡/프로그래머스/덧칠하기.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+
+    int start = section[0];
+    int end = start + m - 1;
+
+    for (int i = 1; i < section.size(); ++i)
+    {
+        if (end >= section[i])
+            continue;
+
+        start = section[i];
+        end = start + m - 1;
+        answer++;
+    }
+    answer++;
+
+
+    return answer;
+}

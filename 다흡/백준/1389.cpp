b028012d4408853ab5a11bc240113ad596fee48b
WhiteCom <vip1408@naver.com>
2023-12-26 00:14:29 +0900

    백준 1389번 문제해결

---
+++ 다흡/백준/1389.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+#include <climits>
+
+#define MAX_COUNT 101
+
+using namespace std;
+
+void InitialGraph(int N, int M, vector< vector<int>>& graph)
+{
+	for (int i = 0; i < M; ++i)
+	{
+		int row, col = 0;
+		cin >> row >> col;
+		graph[row-1][col-1] = 1;
+		graph[col-1][row-1] = 1;
+	}
+}
+
+void FloydWashall(int N, vector< vector<int>>& graph)
+{
+	for (int k = 0; k < N; ++k)
+	{
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			{
+				if (graph[i][j] > graph[i][k] + graph[k][j])
+					graph[i][j] = graph[i][k] + graph[k][j];
+			}
+		}
+	}
+}
+
+int FindShortestPath(int N, vector< vector<int>>& graph)
+{
+	int minKevinBacon = INT_MAX;
+	int result = -1;
+
+	for (int i = 0; i < N; ++i)
+	{
+		int kevinBaconSum = 0;
+		for (int j = 0; j < N; ++j)
+			kevinBaconSum += graph[i][j];
+		
+		if (kevinBaconSum < minKevinBacon)
+		{
+			minKevinBacon = kevinBaconSum;
+			result = i + 1;
+		}
+	}
+
+	return result;
+}
+
+int main()
+{
+	int N, M;
+
+	cin >> N >> M;
+
+	vector< vector<int>> studentGraph(M + 1, vector<int>(N + 1, MAX_COUNT));
+	InitialGraph(N, M, studentGraph);
+
+	FloydWashall(N, studentGraph);
+
+	cout << FindShortestPath(N, studentGraph) << '\n';
+
+	return 0;
+}

20aeba6ba74bf10ff2ed7a54d4aea4c5b21c6e05
WhiteCom <vip1408@naver.com>
2023-12-29 03:00:53 +0900

    해커랭크 Time Conversion문제해결

---
+++ 다흡/해커랭크/Warmup_Time Conversion.cpp
@@ -0,0 +1,49 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+    string result = "";
+    string temp = s.substr(0, 2);
+    int hour = stoi(temp);
+
+    if (s.find("PM") != string::npos
+        && hour < 12)
+    {
+        hour += 12;
+    }
+    else if (s.find("AM") != string::npos
+        && hour >= 12)
+    {
+        hour -= 12;
+    }
+    result = to_string(hour);
+
+    if (result.length() == 1)
+        result = "0" + result;
+
+    return result += s.substr(2, 6);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}

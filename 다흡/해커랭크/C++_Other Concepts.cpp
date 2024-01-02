8b9e00d5f01f44bedad75036702b939c39a004e7
vip1408@naver.com <dhan@masangsoft.com>
2024-01-02 10:15:39 +0900

    해커랭크 Other Concepts 문제해결

---
+++ 다흡/해커랭크/C++_Other Concepts.cpp
@@ -0,0 +1,49 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+template<class T>
+class AddElements
+{
+private:
+    T _a;
+
+public:
+    AddElements(T a) { _a = a; }
+
+    T add(T b) { return _a + b; }
+    T concatenate(T b) { return _a + b; }
+};
+
+int main() {
+    int n, i;
+    cin >> n;
+    for (i = 0; i < n; i++) {
+        string type;
+        cin >> type;
+        if (type == "float") {
+            double element1, element2;
+            cin >> element1 >> element2;
+            AddElements<double> myfloat(element1);
+            cout << myfloat.add(element2) << endl;
+        }
+        else if (type == "int") {
+            int element1, element2;
+            cin >> element1 >> element2;
+            AddElements<int> myint(element1);
+            cout << myint.add(element2) << endl;
+        }
+        else if (type == "string") {
+            string element1, element2;
+            cin >> element1 >> element2;
+            AddElements<string> mystring(element1);
+            cout << mystring.concatenate(element2) << endl;
+        }
+    }
+    return 0;
+}

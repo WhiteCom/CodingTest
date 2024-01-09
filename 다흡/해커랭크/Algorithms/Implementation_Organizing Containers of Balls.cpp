1f9c46bc9b295b233c61680e0e548f929ec407a3
WhiteCom <vip1408@naver.com>
2024-01-09 23:02:35 +0900

    해커랭크 Organizing Containers of Balls 문제해결

---
+++ 다흡/해커랭크/Algorithms/Implementation_Organizing Containers of Balls.cpp
@@ -0,0 +1,122 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string&);
+string rtrim(const string&);
+vector<string> split(const string&);
+
+/*
+ * Complete the 'organizingContainers' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts 2D_INTEGER_ARRAY container as parameter.
+ */
+
+string organizingContainers(vector<vector<int>> container) {
+    int rowSize = container.size();
+    int colSize = container[0].size();
+
+    vector<int> vecRowSum(rowSize, 0);
+    vector<int> vecColSum(colSize, 0);
+
+    for (int i = 0; i < rowSize; ++i)
+    {
+        for (int j = 0; j < colSize; ++j)
+        {
+            vecRowSum[i] += container[i][j];
+            vecColSum[j] += container[i][j];
+        }
+    }
+
+    sort(vecRowSum.begin(), vecRowSum.end());
+    sort(vecColSum.begin(), vecColSum.end());
+
+    for (int i = 0; i < vecRowSum.size(); ++i)
+    {
+        if (vecRowSum[i] != vecColSum[i])
+            return "Impossible";
+    }
+    return "Possible";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        vector<vector<int>> container(n);
+
+        for (int i = 0; i < n; i++) {
+            container[i].resize(n);
+
+            string container_row_temp_temp;
+            getline(cin, container_row_temp_temp);
+
+            vector<string> container_row_temp = split(rtrim(container_row_temp_temp));
+
+            for (int j = 0; j < n; j++) {
+                int container_row_item = stoi(container_row_temp[j]);
+
+                container[i][j] = container_row_item;
+            }
+        }
+
+        string result = organizingContainers(container);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string& str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}

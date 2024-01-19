0176e713e0e14880d5ff190dd1f7fae3734e75b5
WhiteCom <vip1408@naver.com>
2024-01-20 01:40:34 +0900

    codetree 알파벳과 숫자출력2 문제해결

---
+++ 다흡/CodeTree/연습문제_알파벳과 숫자 출력2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char alpha[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    int numbers[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    int n;
+    cin >> n;
+    int alphaIdx = 0;
+    int numberIdx = 0;
+    for (int i = 0; i < n + 1; ++i)
+    {
+        int leftRange = n - i;
+        int rightRange = i;
+        for (int j = 0; j < leftRange; ++j)
+        {
+            cout << alpha[alphaIdx % 26] << " ";
+            alphaIdx++;
+        }
+        for (int j = 0; j < rightRange; ++j)
+        {
+            cout << numbers[numberIdx % 10] << " ";
+            numberIdx++;
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}

aeaf010dfbaa1497dafa537b19fbeed361c96680
dhan <dhan@masangsoft.com>
2024-01-24 13:54:58 +0900

    해커랭크 Minimum Loss 문제해결

---
+++ 다흡/해커랭크/Algorithms/Search_Minimum Loss.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string&);
+string rtrim(const string&);
+vector<string> split(const string&);
+
+/*
+ * Complete the 'minimumLoss' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY price as parameter.
+ */
+
+int minimumLoss(vector<long> price) {
+
+    // key : price value
+    // value : price index
+    vector< pair<long, int>> v;
+    for (size_t i = 0; i < price.size(); ++i)
+    {
+        v.push_back({ price[i], i });
+    }
+    sort(v.begin(), v.end());
+
+    int result = 1000001;
+    for (size_t i = 0; i < v.size() - 1; ++i)
+    {
+        if (v[i].second > v[i + 1].second)
+        {
+            result = result > v[i + 1].first - v[i].first ? v[i + 1].first - v[i].first : result;
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string price_temp_temp;
+    getline(cin, price_temp_temp);
+
+    vector<string> price_temp = split(rtrim(price_temp_temp));
+
+    vector<long> price(n);
+
+    for (int i = 0; i < n; i++) {
+        long price_item = stol(price_temp[i]);
+
+        price[i] = price_item;
+    }
+
+    int result = minimumLoss(price);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string& str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}

d0d3121daf2a480dbd641f216500b3d13d7e9b8f
WhiteCom <vip1408@naver.com>
2024-02-29 00:45:31 +0900

    백준 1003번 문제해결

---
+++ 다흡/백준/1003.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct sData
+{
+	int fiboNumber;
+	int zeroCount;
+	int oneCount;
+
+	sData operator+(const sData& s) const
+	{
+		sData result;
+		result.fiboNumber = this->fiboNumber + s.fiboNumber;
+		result.zeroCount = this->zeroCount + s.zeroCount;
+		result.oneCount = this->oneCount + s.oneCount;
+		return result;
+	}
+};
+
+sData Fibonachi(vector<sData>& arr, int n)
+{
+	if (arr[n].fiboNumber > 0)
+		return arr[n];
+
+	arr[n] = arr[n - 1] + arr[n - 2];
+	return arr[n];
+}
+
+int main()
+{
+	vector<sData> fiboNumbers(41, {0, 0, 0});
+	fiboNumbers[0].fiboNumber = 0;
+	fiboNumbers[0].zeroCount = 1;
+	fiboNumbers[0].oneCount = 0;
+
+	fiboNumbers[1].fiboNumber = 1;
+	fiboNumbers[1].zeroCount = 0;
+	fiboNumbers[1].oneCount = 1;
+
+	for (int i = 2; i <= 40; ++i)
+	{
+		fiboNumbers[i] = Fibonachi(fiboNumbers, i);
+	}
+
+	int testCase;
+	cin >> testCase;
+	for (int i = 0; i < testCase; ++i)
+	{
+		int num;
+		cin >> num;
+
+		cout << fiboNumbers[num].zeroCount << " " << fiboNumbers[num].oneCount << '\n';
+	}
+	return 0;
+}

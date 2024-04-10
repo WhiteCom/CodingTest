c5786adb0d74f0d689cd970d679a2c3016f0b5fb
WhiteCom <vip1408@naver.com>
2024-04-11 01:11:04 +0900

    백준 9461번 cpp 문제해결

---
+++ 다흡/백준/Cpp/9461.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int testCase;
+	cin >> testCase; 
+
+	/*
+	(i >= 5)
+	dp[i] = dp[i-1] + dp[i-5]
+	*/
+
+	vector<long> dp(101, 0);
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 1;
+	dp[3] = 1;
+	dp[4] = 2;
+	for (int i = 5; i <= 100; ++i)
+	{
+		dp[i] = dp[i - 1] + dp[i - 5];
+	}
+
+	for (int i = 0; i < testCase; ++i)
+	{
+		int N;
+		cin >> N;
+		cout << dp[N] << '\n';
+	}
+	return 0;
+}

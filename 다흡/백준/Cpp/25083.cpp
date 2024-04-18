96a0954a5bf94e2b3258eb1a0fa23d7d1a5ee95a
dhan <dhan@masangsoft.com>
2024-04-18 17:36:12 +0900

    백준 25083번 cpp 문제해결

---
+++ 다흡/백준/Cpp/25083.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
+
+int main()
+{
+    cout << "         ,r'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+	return 0;
+}

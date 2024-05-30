c44b1e4acd659da27015d7d1b4f069259f55e755
dhan <dhan@masangsoft.com>
2024-05-30 10:34:36 +0900

    백준 11659번 cpp 2번째 해결

---
+++ 다흡/백준/Cpp/11659_2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	// Input
+	int numCount, testCase;
+	cin >> numCount >> testCase;
+
+	vector<int> numbers(numCount, 0);
+	for (int i = 0; i < numCount; ++i)
+		cin >> numbers[i];
+
+	// 누적합 구하기
+	vector<int> prefixSum(numCount + 1, 0);
+	for (int i = 0; i < numCount; ++i)
+	{
+		prefixSum[i + 1] = prefixSum[i] + numbers[i];
+	}
+
+	// Calculate
+	for (int i = 0; i < testCase; ++i)
+	{
+		int start, end;
+		cin >> start >> end;
+
+		if (start == end)
+			cout << numbers[start - 1] << '\n';
+		else if (start == 1)
+			cout << prefixSum[end] << '\n';
+		else
+			cout << prefixSum[end] - prefixSum[start - 1] << '\n';
+	}
+
+	return 0;
+}
